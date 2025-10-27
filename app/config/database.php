4282b935ef7b342d325979b6cca5b97e951b495a
Lei Gabriel <malibiranlei2005@gmail.com>
2025-10-27 17:03:01 +0800

    update db

--- app/config/database.php
+++ app/config/database.php
@@ -57,6 +57,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |   Example: $database['another_example'] = array('key' => 'value')
 */
 
+/* database localhost
 $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'localhost',
@@ -69,5 +70,17 @@ $database['main'] = array(
     // Optional for SQLite
     'path'      => ''
 );
+*/
 
+// database using environment
+$database['main'] = array(
+    'driver'    => 'mysql',
+    'hostname'  => getenv("DB_HOST"),
+    'port'      => getenv("DB_PORT"),
+    'username'  => getenv("DB_USER"),
+    'password'  => getenv("DB_PASS"),
+    'database'  => getenv("DB_NAME"),
+    'charset'   => 'utf8mb4',
+    'dbprefix'  => ''
+);
 ?>
