a02a46b9fd53d1706fa1e3b7e84de1dcce2651fd
Lei Gabriel <malibiranlei2005@gmail.com>
2025-10-28 20:33:24 +0800

    Fix: Access user data as array instead of object in google_callback

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -1,9 +1,21 @@
 <?php
-defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 
-class Welcome extends Controller {
-	public function index() {
-		$this->call->view('user_landing');
+class Welcome extends Controller
+{
+	public function index()
+	{
+		$data = [];
+
+		$user_email = $this->session->userdata('user_email');
+
+		if ($user_email) {
+			$data['is_logged_in'] = true;
+			$data['user_email'] = $user_email;
+		} else {
+			$data['is_logged_in'] = false;
+		}
+
+		$this->call->view('user_landing', $data);
 	}
 }
-?>
