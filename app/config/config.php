dc6ce30d1b3989b4a40985cc3fc3fd0e360e684c
Lei Gabriel <malibiranlei2005@gmail.com>
2025-10-28 18:17:57 +0800

    config index_page

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://dentalcare-health.onrender.com/';
+$config['base_url'] 				= 'http://dentalcare-health.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -90,7 +90,7 @@ $config['base_url'] 				= 'https://dentalcare-health.onrender.com/';
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '';
 
 /*
 |--------------------------------------------------------------------------
