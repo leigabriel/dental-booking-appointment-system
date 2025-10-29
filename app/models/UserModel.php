d94f9c795c0d9814ca4d6dd02467dfcad3db0143
Lei Gabriel <malibiranlei2005@gmail.com>
2025-10-29 19:04:04 +0800

    fix(usermodel): generate username for google sign-in users

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -22,6 +22,7 @@ class UserModel extends Model
         return $this->filter(['email' => $email])->get();
     }
 
+    // Register user with conditional username generation
     public function register($data)
     {
         if (empty($data['password'])) {
